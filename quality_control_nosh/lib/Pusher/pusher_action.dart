1d6ce5e1f141f81a1c72dcb431435050b474fad2
JOBIN-P-THOMAS <144614482+JOBIN-P-THOMAS@users.noreply.github.com>
2024-04-18 02:23:03 +0530

    error messages done

--- quality_control_nosh/lib/Pusher/pusher_action.dart
+++ quality_control_nosh/lib/Pusher/pusher_action.dart
@@ -1,10 +1,12 @@
-// ignore_for_file: library_private_types_in_public_api, prefer_final_fields, avoid_print, prefer_const_constructors, unused_element, sort_child_properties_last, prefer_const_declarations
+// ignore_for_file: library_private_types_in_public_api, prefer_final_fields, avoid_print, prefer_const_constructors, unused_element, sort_child_properties_last, prefer_const_declarations, sized_box_for_whitespace
 
 import 'dart:async';
 import 'dart:typed_data';
 import 'package:flutter/material.dart';
+import 'package:quality_control_nosh/Pusher/ui.dart';
 import 'package:usb_serial/usb_serial.dart';
 // import 'package:quality_control_nosh/Pusher/ui.dart';
+// import 'package:audioplayers/audioplayers.dart';
 
 class PusherAction extends StatefulWidget {
   final String qrData;
@@ -183,6 +185,8 @@ class _PusherActionState extends State<PusherAction> {
           });
           _scrollToBottom();
           _waitForResponseAfterCmdA002();
+        } else if (response.trim() == '-101') {
+          _stopPlaying();
         }
       }
     });
@@ -193,7 +197,7 @@ class _PusherActionState extends State<PusherAction> {
     bool waitingForResponse = true;
 
     // Start a timer to stop waiting after 6 seconds
-    Timer(Duration(seconds: 6), () {
+    Timer(Duration(seconds: 8), () {
       // Stop waiting after 6 seconds
       waitingForResponse = false;
     });
@@ -221,17 +225,19 @@ class _PusherActionState extends State<PusherAction> {
           });
           _scrollToBottom();
           _waitForResponseAfterCmdA003();
+        } else if (response.trim() == '-101') {
+          _stopPlaying();
         }
       }
     });
   }
 
   void _waitForResponseAfterCmdA003() {
-// Set a flag to indicate waiting for response
+    // Set a flag to indicate waiting for response
     bool waitingForResponse = true;
 
     // Start a timer to stop waiting after 6 seconds
-    Timer(Duration(seconds: 6), () {
+    Timer(Duration(seconds: 8), () {
       // Stop waiting after 6 seconds
       waitingForResponse = false;
     });
@@ -252,6 +258,47 @@ class _PusherActionState extends State<PusherAction> {
 
           // _showCheckContainer(); // Display the "Check 1" container
 
+          final command = 'CMD A 004\r\n';
+          _port!.write(Uint8List.fromList(command.codeUnits));
+          setState(() {
+            _sentCommands.add(command);
+          });
+          _scrollToBottom();
+          _waitForResponseAfterCmdA004();
+        } else if (response.trim() == '-101') {
+          _stopPlaying();
+        }
+      }
+    });
+  }
+
+  void _waitForResponseAfterCmdA004() {
+    // Set a flag to indicate waiting for response
+    bool waitingForResponse = true;
+
+    // Start a timer to stop waiting after 6 seconds
+    Timer(Duration(seconds: 8), () {
+      // Stop waiting after 6 seconds
+      waitingForResponse = false;
+    });
+
+    _port!.inputStream!.listen((Uint8List data) async {
+      if (!waitingForResponse) return; // Ignore responses after 6 seconds
+
+      if (data.isNotEmpty) {
+        final response = String.fromCharCodes(data);
+        print('Received response: $response');
+
+        if (response.trim() == '0') {
+          // Received expected response, stop waiting
+          waitingForResponse = false;
+
+          // Process the response or perform any necessary actions
+          print('Received expected response: $response');
+          _stopPlaying();
+
+          // _showCheckContainer(); // Display the "Check 1" container
+
           // final command = 'CMD A 003\r\n';
           // _port!.write(Uint8List.fromList(command.codeUnits));
           // setState(() {
@@ -259,29 +306,90 @@ class _PusherActionState extends State<PusherAction> {
           // });
           // _scrollToBottom();
           // _waitForResponseAfterCmdA003();
+        } else if (response.trim() == '-101') {
+          _stopPlaying();
         }
       }
     });
   }
 
-  void _showCheckContainer() {
-    showDialog(
-      context: context,
-      builder: (context) => AlertDialog(
-        title: Text('Check 1'),
-        actions: [
-          ElevatedButton(
-            onPressed: () {
-              Navigator.of(context).pop(); // Close the dialog
-            },
-            child: Text('Close'),
-          ),
-        ],
+  void _stopPlaying() {
+    // Show a snackbar indicating assembly failure
+    ScaffoldMessenger.of(context).showSnackBar(
+      SnackBar(
+        content: Text(
+          'Assembly Failed',
+          style: TextStyle(color: Colors.white),
+        ),
+        backgroundColor: Colors.red,
+        duration: Duration(seconds: 3),
       ),
     );
-  }
 
-  void _stopPlaying() {}
+    // Show an alert dialog displaying sent and received commands
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: Text(
+            widget.qrData,
+            style: TextStyle(fontWeight: FontWeight.bold),
+          ),
+          content: Container(
+            width: double.maxFinite,
+            height: 300,
+            child: ListView.builder(
+              itemCount: _sentCommands.length + _receivedCommands.length,
+              itemBuilder: (context, index) {
+                if (index % 2 == 0) {
+                  // Display sent command
+                  final sentIndex = index ~/ 2;
+                  if (sentIndex < _sentCommands.length) {
+                    final sentCommand = _sentCommands[sentIndex];
+                    if (sentCommand.isNotEmpty) {
+                      return ListTile(
+                        title: Text(
+                          'Sent: $sentCommand',
+                          style: TextStyle(color: Colors.blue),
+                        ),
+                      );
+                    }
+                  }
+                } else {
+                  // Display received response
+                  final receivedIndex = index ~/ 2;
+                  if (receivedIndex < _receivedCommands.length) {
+                    final receivedCommand = _receivedCommands[receivedIndex];
+                    if (receivedCommand.isNotEmpty) {
+                      return ListTile(
+                        title: Text(
+                          'Received: $receivedCommand',
+                          style: TextStyle(color: Colors.green),
+                        ),
+                      );
+                    }
+                  }
+                }
+                return SizedBox.shrink();
+              },
+            ),
+          ),
+          actions: [
+            TextButton(
+              onPressed: () {
+                Navigator.of(context).pop(); // Close the dialog
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => MyPusher()),
+                ); // Navigate to MyPusher page
+              },
+              child: Text('OK', style: TextStyle(color: Colors.red)),
+            ),
+          ],
+        );
+      },
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -289,47 +397,48 @@ class _PusherActionState extends State<PusherAction> {
       appBar: AppBar(
         title: Text(widget.qrData),
       ),
-      body: ListView(
+      body: Column(
         children: [
           SizedBox(height: 20), // Add spacing
+
           // Scrollable container for commands
-          Container(
-            height: 400, // Set a fixed height or adjust as needed
-            padding: EdgeInsets.all(10),
-            decoration: BoxDecoration(
-              border: Border.all(color: Colors.grey),
-              borderRadius: BorderRadius.circular(10),
-            ),
-            child: ListView.builder(
-              controller: _scrollController,
-              itemCount: _sentCommands.length + _receivedCommands.length,
-              itemBuilder: (context, index) {
-                if (index < _sentCommands.length) {
-                  final sentCommand = _sentCommands[index];
-                  if (sentCommand.isNotEmpty) {
-                    return ListTile(
-                      title: Text('Sent: $sentCommand'),
-                    );
-                  } else {
-                    return SizedBox.shrink(); // Skip empty sent commands
-                  }
-                } else {
-                  final receivedIndex = index - _sentCommands.length;
-                  if (receivedIndex >= 0 &&
-                      receivedIndex < _receivedCommands.length) {
-                    final receivedCommand = _receivedCommands[receivedIndex];
-                    if (receivedCommand.isNotEmpty) {
-                      return ListTile(
-                        title: Text('Received: $receivedCommand'),
-                      );
-                    } else {
-                      return SizedBox.shrink(); // Skip empty received commands
+          Expanded(
+            child: Container(
+              padding: EdgeInsets.all(10),
+              decoration: BoxDecoration(
+                border: Border.all(color: Colors.grey),
+                borderRadius: BorderRadius.circular(10),
+              ),
+              child: ListView.builder(
+                controller: _scrollController,
+                itemCount: _sentCommands.length + _receivedCommands.length,
+                itemBuilder: (context, index) {
+                  if (index % 2 == 0) {
+                    // Display sent command
+                    final sentIndex = index ~/ 2;
+                    if (sentIndex < _sentCommands.length) {
+                      final sentCommand = _sentCommands[sentIndex];
+                      if (sentCommand.isNotEmpty) {
+                        return ListTile(
+                          title: Text('Sent: $sentCommand'),
+                        );
+                      }
                     }
                   } else {
-                    return SizedBox.shrink();
+                    // Display received response
+                    final receivedIndex = index ~/ 2;
+                    if (receivedIndex < _receivedCommands.length) {
+                      final receivedCommand = _receivedCommands[receivedIndex];
+                      if (receivedCommand.isNotEmpty) {
+                        return ListTile(
+                          title: Text('Received: $receivedCommand'),
+                        );
+                      }
+                    }
                   }
-                }
-              },
+                  return SizedBox.shrink();
+                },
+              ),
             ),
           ),
         ],
@@ -357,12 +466,8 @@ class _PusherActionState extends State<PusherAction> {
                 _stopPlaying();
               }
             },
-            backgroundColor: _isPlaying
-                ? Colors.red
-                : Colors.green, // Change button color based on state
-            child: Icon(_isPlaying
-                ? Icons.stop
-                : Icons.play_arrow), // Change icon based on state
+            backgroundColor: _isPlaying ? Colors.red : Colors.green,
+            child: Icon(_isPlaying ? Icons.stop : Icons.play_arrow),
             heroTag: null,
           ),
         ],
