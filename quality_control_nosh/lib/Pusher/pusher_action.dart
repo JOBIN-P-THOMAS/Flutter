f7cd27244ac02db6070a5f78cd527d3dea98b284
JOBIN-P-THOMAS <144614482+JOBIN-P-THOMAS@users.noreply.github.com>
2024-05-01 20:54:43 +0530

    26 sec timer set

--- quality_control_nosh/lib/Pusher/pusher_action.dart
+++ quality_control_nosh/lib/Pusher/pusher_action.dart
@@ -1,4 +1,4 @@
-// ignore_for_file: library_private_types_in_public_api, prefer_final_fields, avoid_print, prefer_const_constructors, unused_element, sort_child_properties_last, prefer_const_declarations, sized_box_for_whitespace
+// ignore_for_file: library_private_types_in_public_api, prefer_final_fields, avoid_print, prefer_const_constructors, unused_element, sort_child_properties_last, prefer_const_declarations, sized_box_for_whitespace, use_build_context_synchronously, prefer_const_literals_to_create_immutables, depend_on_referenced_packages, unnecessary_import, unused_import
 
 import 'dart:async';
 import 'dart:typed_data';
@@ -201,52 +201,14 @@ class _PusherActionState extends State<PusherAction> {
     // Set a flag to indicate waiting for response
     bool waitingForResponse = true;
 
-    // Start a timer to stop waiting after 6 seconds
-    Timer(Duration(seconds: 6), () {
-      // Stop waiting after 6 seconds
-      waitingForResponse = false;
-    });
-
-    _port!.inputStream!.listen((Uint8List data) async {
-      if (!waitingForResponse || !mounted)
-        return; // Check mounted before setState
-
-      if (data.isNotEmpty) {
-        final response = String.fromCharCodes(data).trim();
-        print('Received response: $response');
-
-        if (response.trim() == '0') {
-          // Received expected response, stop waiting
-          waitingForResponse = false;
-
-          // Process the response or perform any necessary actions
-          print('Received expected response: $response');
-
-          // _showCheckContainer(); // Display the "Check 1" container
-          await Future.delayed(Duration(seconds: 1));
-          final command = 'CMD A 002\r\n';
-          _port!.write(Uint8List.fromList(command.codeUnits));
-          setState(() {
-            _sentCommands.add(command);
-          });
-          _scrollToBottom();
-          _waitForResponseAfterCmdA002();
-        } else if (response.trim() == '-101') {
-          _stopPlaying();
-        }
+    // Start a timer to stop waiting after 26 seconds
+    Timer timeoutTimer = Timer(Duration(seconds: 26), () {
+      if (waitingForResponse && mounted) {
+        // If no response has been received and the widget is still mounted
+        print('Response timeout, stopping playback');
+        _stopPlaying(); // Stop playing if timeout occurs
       }
     });
-  }
-
-  void _waitForResponseAfterCmdA002() {
-    // Set a flag to indicate waiting for response
-    bool waitingForResponse = true;
-
-    // Start a timer to stop waiting after 6 seconds
-    Timer(Duration(seconds: 8), () {
-      // Stop waiting after 6 seconds
-      waitingForResponse = false;
-    });
 
     _port!.inputStream!.listen((Uint8List data) async {
       if (!waitingForResponse || !mounted)
@@ -256,108 +218,16 @@ class _PusherActionState extends State<PusherAction> {
         final response = String.fromCharCodes(data).trim();
         print('Received response: $response');
 
-        if (response.trim() == '0') {
-          // Received expected response, stop waiting
+        if (response.trim() == '-101') {
+          // Received response code '-101', stop waiting and trigger _stopPlaying()
           waitingForResponse = false;
-
-          // Process the response or perform any necessary actions
-          print('Received expected response: $response');
-
-          // _showCheckContainer(); // Display the "Check 1" container
-          await Future.delayed(Duration(seconds: 1));
-          final command = 'CMD A 003\r\n';
-          _port!.write(Uint8List.fromList(command.codeUnits));
-          setState(() {
-            _sentCommands.add(command);
-          });
-          _scrollToBottom();
-          _waitForResponseAfterCmdA003();
-        } else if (response.trim() == '-101') {
-          _stopPlaying();
-        }
-      }
-    });
-  }
-
-  void _waitForResponseAfterCmdA003() {
-    // Set a flag to indicate waiting for response
-    bool waitingForResponse = true;
-
-    // Start a timer to stop waiting after 6 seconds
-    Timer(Duration(seconds: 8), () {
-      // Stop waiting after 6 seconds
-      waitingForResponse = false;
-    });
-
-    _port!.inputStream!.listen((Uint8List data) async {
-      if (!waitingForResponse || !mounted)
-        return; // Check mounted before setState
-
-      if (data.isNotEmpty) {
-        final response = String.fromCharCodes(data).trim();
-        print('Received response: $response');
-
-        if (response.trim() == '0') {
-          // Received expected response, stop waiting
+          timeoutTimer.cancel(); // Cancel the timeout timer
+          _stopPlaying(); // Trigger stop playing action
+        } else if (response.trim() == '0') {
+          // Received response code '0', stop waiting and trigger _success()
           waitingForResponse = false;
-
-          // Process the response or perform any necessary actions
-          print('Received expected response: $response');
-
-          // _showCheckContainer(); // Display the "Check 1" container
-          await Future.delayed(Duration(seconds: 1));
-          final command = 'CMD A 004\r\n';
-          _port!.write(Uint8List.fromList(command.codeUnits));
-          setState(() {
-            _sentCommands.add(command);
-          });
-          _scrollToBottom();
-          _waitForResponseAfterCmdA004();
-        } else if (response.trim() == '-101') {
-          _stopPlaying();
-        }
-      }
-    });
-  }
-
-  void _waitForResponseAfterCmdA004() {
-    // Set a flag to indicate waiting for response
-    bool waitingForResponse = true;
-
-    // Start a timer to stop waiting after 6 seconds
-    Timer(Duration(seconds: 8), () {
-      // Stop waiting after 6 seconds
-      waitingForResponse = false;
-    });
-
-    _port!.inputStream!.listen((Uint8List data) async {
-      if (!waitingForResponse || !mounted)
-        return; // Check mounted before setState
-
-      if (data.isNotEmpty) {
-        final response = String.fromCharCodes(data).trim();
-        print('Received response: $response');
-        await Future.delayed(Duration(seconds: 1));
-
-        if (response.trim() == '0') {
-          // Received expected response, stop waiting
-          waitingForResponse = false;
-
-          // Process the response or perform any necessary actions
-          print('Received expected response: $response');
-          _stopPlaying();
-
-          // _showCheckContainer(); // Display the "Check 1" container
-
-          // final command = 'CMD A 003\r\n';
-          // _port!.write(Uint8List.fromList(command.codeUnits));
-          // setState(() {
-          //   _sentCommands.add(command);
-          // });
-          // _scrollToBottom();
-          // _waitForResponseAfterCmdA003();
-        } else if (response.trim() == '-101') {
-          _stopPlaying();
+          timeoutTimer.cancel(); // Cancel the timeout timer
+          _success(); // Trigger success action
         }
       }
     });
@@ -516,6 +386,159 @@ class _PusherActionState extends State<PusherAction> {
     );
   }
 
+  void _success() async {
+    try {
+      final directory = Directory('/storage/emulated/0/Download');
+
+      // Check if the Download directory exists, create it if not
+      if (!await directory.exists()) {
+        await directory.create(recursive: true);
+      }
+
+      final qrCodeData = widget.qrData;
+      final now = DateTime.now();
+      final timestamp =
+          '${now.year}-${now.month}-${now.day}_${now.hour}-${now.minute}-${now.second}';
+      // final timestamp = DateTime.now().minute;
+      String fileName = '$qrCodeData-$timestamp.txt';
+      String fileNameAWS = '$qrCodeData-$timestamp';
+      // String fileName = '$qrCodeData.txt';
+      String filePath = '${directory.path}/$fileName';
+
+      // Check if the file already exists in the Download directory
+      int count = 1;
+      while (await File(filePath).exists()) {
+        // File already exists, generate a new file name with a count
+        fileName = '$qrCodeData-$count.txt';
+        filePath = '${directory.path}/$fileName';
+        count++;
+      }
+
+      final file = File(filePath);
+
+      String content = 'Sent Commands:\n';
+      for (final command in _sentCommands) {
+        content += '$command\n';
+      }
+      content += '\nReceived Commands:\n';
+      for (final command in _receivedCommands) {
+        content += '$command\n';
+      }
+
+      await file.writeAsString(content);
+
+      print('Commands saved to $filePath');
+
+      final apiUrl =
+          'https://fls8oe8xp7.execute-api.ap-south-1.amazonaws.com/dev/nosh-test-S3?file_name=$fileNameAWS';
+      final response = await http.post(
+        Uri.parse(apiUrl),
+        body: content,
+        headers: {
+          'Content-Type': 'text/plain'
+        }, // Specify content type as text/plain
+      );
+
+      if (response.statusCode == 200) {
+        print('Commands sent to S3 successfully');
+      } else {
+        print(
+            'Failed to send commands to S3. Status code: ${response.statusCode}');
+      }
+    } catch (e) {
+      print('Error saving and sending commands: $e');
+    }
+
+    // Show a custom dialog indicating assembly failure
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return Dialog(
+          child: Container(
+            padding: EdgeInsets.all(20),
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                Icon(
+                  Icons.check,
+                  size: 60,
+                  color: Colors.green,
+                ),
+                SizedBox(height: 20),
+                Text(
+                  'Assembly Success',
+                  style: TextStyle(
+                    fontSize: 18,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.green,
+                  ),
+                ),
+                SizedBox(height: 20),
+                Text(
+                  'QR Data: ${widget.qrData}',
+                  style: TextStyle(fontSize: 16),
+                ),
+                SizedBox(height: 20),
+                Text(
+                  'Sent Commands:',
+                  style: TextStyle(fontWeight: FontWeight.bold),
+                ),
+                SizedBox(height: 10),
+                Expanded(
+                  child: ListView.builder(
+                    itemCount: _sentCommands.length,
+                    itemBuilder: (context, index) {
+                      final sentCommand = _sentCommands[index];
+                      return ListTile(
+                        title: Text(
+                          'Sent: $sentCommand',
+                          style: TextStyle(color: Colors.blue),
+                        ),
+                      );
+                    },
+                  ),
+                ),
+                SizedBox(height: 20),
+                Text(
+                  'Received Commands:',
+                  style: TextStyle(fontWeight: FontWeight.bold),
+                ),
+                SizedBox(height: 10),
+                Expanded(
+                  child: ListView.builder(
+                    itemCount: _receivedCommands.length,
+                    itemBuilder: (context, index) {
+                      final receivedCommand = _receivedCommands[index];
+                      return ListTile(
+                        title: Text(
+                          'Received: $receivedCommand',
+                          style: TextStyle(color: Colors.green),
+                        ),
+                      );
+                    },
+                  ),
+                ),
+                SizedBox(height: 20),
+                TextButton(
+                  onPressed: () {
+                    Navigator.of(context).pop(); // Close the dialog
+                    Navigator.pushAndRemoveUntil(
+                      context,
+                      MaterialPageRoute(builder: (context) => MyPusher()),
+                      (route) => false, // Remove all routes until the new route
+                    );
+                    // Navigate to MyPusher page
+                  },
+                  child: Text('OK', style: TextStyle(color: Colors.red)),
+                ),
+              ],
+            ),
+          ),
+        );
+      },
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -599,3 +622,134 @@ class _PusherActionState extends State<PusherAction> {
     );
   }
 }
+
+
+
+
+
+
+
+  // void _waitForResponseAfterCmdA002() {
+  //   // Set a flag to indicate waiting for response
+  //   bool waitingForResponse = true;
+
+  //   // Start a timer to stop waiting after 6 seconds
+  //   Timer(Duration(seconds: 8), () {
+  //     // Stop waiting after 6 seconds
+  //     waitingForResponse = false;
+  //   });
+
+  //   _port!.inputStream!.listen((Uint8List data) async {
+  //     if (!waitingForResponse || !mounted)
+  //       return; // Check mounted before setState
+
+  //     if (data.isNotEmpty) {
+  //       final response = String.fromCharCodes(data).trim();
+  //       print('Received response: $response');
+
+  //       if (response.trim() == '0') {
+  //         // Received expected response, stop waiting
+  //         waitingForResponse = false;
+
+  //         // Process the response or perform any necessary actions
+  //         print('Received expected response: $response');
+
+  //         // _showCheckContainer(); // Display the "Check 1" container
+  //         await Future.delayed(Duration(seconds: 1));
+  //         final command = 'CMD A 003\r\n';
+  //         _port!.write(Uint8List.fromList(command.codeUnits));
+  //         setState(() {
+  //           _sentCommands.add(command);
+  //         });
+  //         _scrollToBottom();
+  //         _waitForResponseAfterCmdA003();
+  //       } else if (response.trim() == '-101') {
+  //         _stopPlaying();
+  //       }
+  //     }
+  //   });
+  // }
+
+  // void _waitForResponseAfterCmdA003() {
+  //   // Set a flag to indicate waiting for response
+  //   bool waitingForResponse = true;
+
+  //   // Start a timer to stop waiting after 6 seconds
+  //   Timer(Duration(seconds: 8), () {
+  //     // Stop waiting after 6 seconds
+  //     waitingForResponse = false;
+  //   });
+
+  //   _port!.inputStream!.listen((Uint8List data) async {
+  //     if (!waitingForResponse || !mounted)
+  //       return; // Check mounted before setState
+
+  //     if (data.isNotEmpty) {
+  //       final response = String.fromCharCodes(data).trim();
+  //       print('Received response: $response');
+
+  //       if (response.trim() == '0') {
+  //         // Received expected response, stop waiting
+  //         waitingForResponse = false;
+
+  //         // Process the response or perform any necessary actions
+  //         print('Received expected response: $response');
+
+  //         // _showCheckContainer(); // Display the "Check 1" container
+  //         await Future.delayed(Duration(seconds: 1));
+  //         final command = 'CMD A 004\r\n';
+  //         _port!.write(Uint8List.fromList(command.codeUnits));
+  //         setState(() {
+  //           _sentCommands.add(command);
+  //         });
+  //         _scrollToBottom();
+  //         _waitForResponseAfterCmdA004();
+  //       } else if (response.trim() == '-101') {
+  //         _stopPlaying();
+  //       }
+  //     }
+  //   });
+  // }
+
+  // void _waitForResponseAfterCmdA004() {
+  //   // Set a flag to indicate waiting for response
+  //   bool waitingForResponse = true;
+
+  //   // Start a timer to stop waiting after 6 seconds
+  //   Timer(Duration(seconds: 8), () {
+  //     // Stop waiting after 6 seconds
+  //     waitingForResponse = false;
+  //   });
+
+  //   _port!.inputStream!.listen((Uint8List data) async {
+  //     if (!waitingForResponse || !mounted)
+  //       return; // Check mounted before setState
+
+  //     if (data.isNotEmpty) {
+  //       final response = String.fromCharCodes(data).trim();
+  //       print('Received response: $response');
+  //       await Future.delayed(Duration(seconds: 1));
+
+  //       if (response.trim() == '0') {
+  //         // Received expected response, stop waiting
+  //         waitingForResponse = false;
+
+  //         // Process the response or perform any necessary actions
+  //         print('Received expected response: $response');
+         
+
+  //         // _showCheckContainer(); // Display the "Check 1" container
+
+  //         // final command = 'CMD A 003\r\n';
+  //         // _port!.write(Uint8List.fromList(command.codeUnits));
+  //         // setState(() {
+  //         //   _sentCommands.add(command);
+  //         // });
+  //         // _scrollToBottom();
+  //         // _waitForResponseAfterCmdA003();
+  //       } else if (response.trim() == '-101') {
+  //         _stopPlaying();
+  //       }
+  //     }
+  //   });
+  // }
